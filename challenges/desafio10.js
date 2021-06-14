c47333122b0a3dfc1a0db42a175f15daf77c381a
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-14 15:24:54 -0300

    Desafio 11 concluido

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,10 +1,10 @@
 db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
 } });
 
-db.produtos.updateOne({ nome: { $eq: "Big Mac" } }, { $set: { vendasPorDia: [0, 0, 0, 60, 0, 0, 0],
-} });
+db.produtos.updateOne({ nome: { $eq: "Big Mac" } },
+{ $inc: { "vendasPorDia.3": 60 } });
 
-db.produtos.updateMany({ tags: { $elemMatch: { $in: ["bovino", "pão"] } } },
-{ $set: { vendasPorDia: [0, 0, 0, 60, 0, 0, 120] } });
+db.produtos.updateMany({ tags: { $all: ["bovino", "pão"] } },
+{ $inc: { "vendasPorDia.6": 120 } });
 
 db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
